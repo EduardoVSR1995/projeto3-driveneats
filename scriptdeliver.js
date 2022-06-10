4261a44602f7e854f40fa9d2405cd1c458780282
eduardo <eduardvitor7@gmail.com>
2022-06-10 13:31:11 -0300

    inicio js

--- scriptdeliver.js
+++ scriptdeliver.js
@@ -1,6 +1,20 @@
-function seleciona(){
-    
-    const box = document.querySelector(".box");
-    box.classList.toggle("margem");
-    console.log(box);
+function seleciona(rec){
+    const box = document.querySelector(".prato").children;
+    console.log(box[rec-1].classList.add('margem'));
+    let bot = box[rec-1].querySelector(".botve");
+    console.log(bot.classList.remove('a'));
+
+
+}
+
+ function seleciona1(rec1){
+    const box = document.querySelector(".prato1").children;
+    let bot = box[rec1-1].querySelector(".botve");
+    console.log(bot.classList.remove('a'));
+}
+
+function seleciona2(rec2){
+    const box = document.querySelector(".prato2").children;
+    let bot = box[rec2-1].querySelector(".botve");
+    console.log(bot.classList.remove('a'));
 }
