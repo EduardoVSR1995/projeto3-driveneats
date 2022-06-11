bdf6878638a28c82b06e58b5d0c32e572af7b970
eduardo <eduardvitor7@gmail.com>
2022-06-11 14:03:19 -0300

    botao pedido js

--- scriptdeliver.js
+++ scriptdeliver.js
@@ -1,4 +1,7 @@
-let re=0;
+let a=0;
+let b=0;
+let c=0;
+
 function seleciona(rec){
     const box = document.querySelector(".prato").children;
     box[rec-1].classList.add('margem');
@@ -10,11 +13,10 @@ function seleciona(rec){
             let bo = box[i].querySelector(".botve");
             bo.classList.add('a');
         }
-        else{
-            re++;
-        }
+        
     }
-    
+a=1    
+botaoFinal(a,b,c);
 }
 function seleciona1(rec1){
     const box = document.querySelector(".prato1").children;
@@ -27,11 +29,9 @@ function seleciona1(rec1){
             let bo = box[i].querySelector(".botve");
             bo.classList.add('a');
         }
-    
-    else{
-        re++;
-    }
 }
+b=1;
+botaoFinal(a,b,c);
 }
 
 function seleciona2(rec2){
@@ -45,13 +45,15 @@ function seleciona2(rec2){
             let bo = box[i].querySelector(".botve");
             bo.classList.add('a');
         }
-        else{
-            re++;
+        
+    }
+    c=1;
+    botaoFinal(a,b,c);
+}
+  function botaoFinal(element){
+        if(a===1 && b===1 && c===1 ){
+         const pedido = document.querySelector('.botao');
+        pedido.classList.add('a');
+        console.log(pedido);
         }
     }
-    console.log(re);
-}
-function square(numero) {
-    return numero * numero;
-  }
-  
